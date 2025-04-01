6d3ee9e4fd50f993a6a3a6a4f305d57f9397f6ea
vijayvenkatj <vijayvenkatj@gmail.com>
2025-04-01 18:55:56 +0530

    Fixed WebHook Bug (Wrong param)

--- src/claim_analysis/claimRouter.ts
+++ src/claim_analysis/claimRouter.ts
@@ -12,7 +12,7 @@ export const claimRouter = express.Router();
 
 claimRouter.post('/', async (req, res) => {
     console.log("Request Received",req.body.record)
-    const result = await update_claim(req.body.record.claimId);
+    const result = await update_claim(req.body.record.id);
     console.log(result)
     res.send(result);
 });
