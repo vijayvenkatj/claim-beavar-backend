3585527a881e98e06421794bf76a40fdf1304052
vijayvenkatj <vijayvenkatj@gmail.com>
2025-04-01 18:53:40 +0530

    Added DEBUG logic

--- src/claim_analysis/claimRouter.ts
+++ src/claim_analysis/claimRouter.ts
@@ -11,7 +11,7 @@ export const claimRouter = express.Router();
 
 
 claimRouter.post('/', async (req, res) => {
-    console.log("Request Received")
+    console.log("Request Received",req.body.record)
     const result = await update_claim(req.body.record.claimId);
     console.log(result)
     res.send(result);
