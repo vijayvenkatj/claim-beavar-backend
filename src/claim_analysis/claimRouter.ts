cceef4b63f0a7870c037992518d2dcad9a7cb521
vijayvenkatj <vijayvenkatj@gmail.com>
2025-04-02 12:54:22 +0530

    Added redis to ClaimAnalysis

--- src/claim_analysis/claimRouter.ts
+++ src/claim_analysis/claimRouter.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import { update_claim } from './claimAnalyser';
+import { redisClient } from '../utils/redis/init';
 
 
 const app = express();
@@ -15,4 +16,23 @@ claimRouter.post('/', async (req, res) => {
     const result = await update_claim(req.body.record.id);
     console.log(result)
     res.send(result);
+});
+
+claimRouter.post('/policy_update', async (req, res) => {
+    console.log("Request Received");
+    const claimId = req.body.record.id;
+    const cacheKey = `Policy:${claimId}`
+    await redisClient.del(cacheKey)
+    console.log("Deleted Policy Cache")
+    res.send("updated policy")
+});
+
+
+claimRouter.post('/user_update', async (req, res) => {
+    console.log("Request Received");
+    const policyHolderId = req.body.record.id;
+    const cacheKey = `User:${policyHolderId}`;
+    await redisClient.del(cacheKey)
+    console.log("Deleted PolicyHolder Cache")
+    res.send("updated user")
 });
